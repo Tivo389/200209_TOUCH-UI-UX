7579c858951ba38085705a185be4b5df2c84e19f
Shun Miyaki <shunmiyaki@hotmail.com>
2020-02-17 21:29:41 -0800

    200217 CSS and XY Axis set

--- src/components/DeviceEventConfirmation.js
+++ src/components/DeviceEventConfirmation.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 
 class DeviceEventConfirmation extends Component {
+  // COMPONENT VARIALBES
+  state = {
+    clientX: 0,
+    clientY: 0
+  }
+
   // LIFECYCLE METHODS
-  componentWillMount() {}
-  componentDidMount() {}
-  componentWillUpdate() {}
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+  }
   componentDidUpdate() {}
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    window.addEventListener('resize', this.handleResize);
+  }
 
   // RENDER OF COMPONENT
   render() {
     return (
-      <div className="componentWrapper">
-        <div className="statusContainer">
-          <p className="statusElement">Touch Device</p>
-          <p className="statusElement">Cursor Device</p>
-          <p className="statusElement">X: ---.--</p>
-          <p className="statusElement">Y: ---.--</p>
+      <div className="mainWrapper"
+        onTouchStart={this.handleGesture}
+        onClick={this.handleGesture}
+        onTouchEnd={e => e.preventDefault()}>
+        <div className="componentWrapper">
+          <div className="statusContainer">
+            <p className="statusElement cssPointerCoarse">CSS / any-pointer: coarse</p>
+            <p className="statusElement cssPointerFine">CSS / any-pointer: fine</p>
+            <p className="statusElement jsTouch">JS / Touch Gesture</p>
+            <p className="statusElement jsMouse">JS / Mouse Gesture</p>
+            <p className="statusElement active">X: {this.state.clientX}</p>
+            <p className="statusElement active">Y: {this.state.clientY}</p>
+          </div>
         </div>
       </div>
     );
@@ -24,8 +39,37 @@ class DeviceEventConfirmation extends Component {
 
   // FUNCTION: BASIC EXPLANATION HERE
   // - Detailed explanation here
-  sampleFunction = () => {
-    console.log('sampleFunction!');
+  handleGesture = (e) => {
+    if (e.touches && e.touches.length > 1) {
+      return;
+    } else if (window.PointerEvent) {
+      if (e.targetTouches) {
+        console.log('POINTER EVENT / TOUCH ----------');
+        // console.log(`X-AXIS: ${e.targetTouches[0].clientX}`);
+        // console.log(`Y-AXIS: ${e.targetTouches[0].clientY}`);
+        const xAxis = Math.round(e.targetTouches[0].clientX);
+        const yAxis = Math.round(e.targetTouches[0].clientY);
+        this.setCoordinates(xAxis, yAxis);
+      } else {
+        console.log('POINTER EVENT / MOUSE ----------');
+        // console.log(`X-AXIS: ${e.clientX}`);
+        // console.log(`Y-AXIS: ${e.clientY}`);
+        const xAxis = Math.round(e.clientX);
+        const yAxis = Math.round(e.clientY);
+        this.setCoordinates(xAxis, yAxis);
+      }
+    } else {
+      // WILL WORK ON THIS LAST BUT NOT SURE HOW TO TEST THIS...
+      console.log('STANDARD EVENT / TOUCH OR MOUSE ----------');
+      console.log(`X-AXIS: ${e.clientX}`);
+      console.log(`Y-AXIS: ${e.clientY}`);
+    }
+  };
+  setCoordinates = (xAxis, yAxis) => {
+    this.setState({
+      clientX: xAxis,
+      clientY: yAxis
+    });
   };
 }
 
