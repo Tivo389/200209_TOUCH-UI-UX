2ea7cf9fbf4a25b2d974afccafaa9d4f074db0fd
Shun Miyaki <shunmiyaki@hotmail.com>
2020-02-23 16:38:22 -0800

    200223 Button event component created and added

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
-import DeviceEventConfirmation from './DeviceEventConfirmation';
+import DeviceEvent from './DeviceEvent';
+import ButtonEvent from './ButtonEvent';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={App}/>
-      <Route exact path="/deviceEventConfirmation" component={DeviceEventConfirmation}/>
+      <Route exact path="/deviceEvent" component={DeviceEvent}/>
+      <Route exact path="/buttonEvent" component={ButtonEvent}/>
     </Switch>
   </BrowserRouter>
 );
