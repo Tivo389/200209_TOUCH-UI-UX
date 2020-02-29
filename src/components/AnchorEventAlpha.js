f2984be0ec3148ea0d6a2f2dc1b07e1217a480cc
Shun Miyaki <shunmiyaki@hotmail.com>
2020-02-29 01:07:44 -0800

    200229 Standard CTA stlye updated. Continue by refactoring the commenting code. Should be a component.

--- src/components/AnchorEventAlpha.js
+++ src/components/AnchorEventAlpha.js
@@ -3,8 +3,16 @@ import AnchorEventComponent from './AnchorEventComponent';
 
 class AnchorEventAlpha extends Component {
   render() {
+    const standardCopy = [
+      '200228 / :active works as expected on desktop browsers.',
+      '200228 / :active works as expected only on Firefox in mobile.',
+      '200228 / :active does not reflect on Chrome and Safari in mobile.'
+    ];
     return (
-      <AnchorEventComponent name="Alpha" href="Beta"/>
+      <AnchorEventComponent
+        name="Alpha"
+        href="Beta"
+        standardCopy={standardCopy}/>
     );
   }
 }
