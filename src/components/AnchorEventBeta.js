14726d53f85a06b0a7013b03e19072ed4a371d51
Shun Miyaki <shunmiyaki@hotmail.com>
2020-03-01 23:14:37 -0800

    200301 Feedback component created. Continue with adding any-hover and any-pointer to the Device Event Confirmation.

--- src/components/AnchorEventBeta.js
+++ src/components/AnchorEventBeta.js
@@ -3,16 +3,8 @@ import AnchorEventComponent from './AnchorEventComponent';
 
 class AnchorEventBeta extends Component {
   render() {
-    const standardCopy = [
-      '200228 / :active works as expected on desktop browsers.',
-      '200228 / :active works as expected only on Firefox in mobile.',
-      '200228 / :active does not reflect on Chrome and Safari in mobile.'
-    ];
     return (
-      <AnchorEventComponent
-        name="Beta"
-        href="Alpha"
-        standardCopy={standardCopy}/>
+      <AnchorEventComponent name="Beta" href="Alpha"/>
     );
   }
 }
--- src/components/AnchorEventComponent.js
+++ src/components/AnchorEventComponent.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import Feedback from './Feedback';
 
 class AnchorEventComponent extends Component {
   render() {
-    const {href, name, standardCopy} = this.props;
+    const {href, name} = this.props;
+    const standardCTAFeedback = [
+      '200228 / This CTA takes the conventional focus, hover, active flow.',
+      '200228 / :active is reflected on desktop CFS browsers.',
+      '200228 / :active is reflected only in Firefox in mobile.',
+      '200228 / :active is not reflected in Chrome and Safari mobile.',
+      '200301 / :focus is reflected in desktop',
+      '200301 / :focus is not reflected in mobile',
+      '200301 / :hover is reflected in desktop',
+      '200301 / :hover is reflected in mobile. However, if hover css media feature is used it is (understandably) not reflected. Using hover css media offered better consistency in mobile.',
+    ];
     return (
       <div className="mainWrapper" style={{ height: window.innerHeight }}>
         <div className="anchorEventWrapper">
@@ -12,11 +23,7 @@ class AnchorEventComponent extends Component {
                 Move to {href}
             </a>
             <p>Standard CTA</p>
-            <ul>
-              {standardCopy.map((element, key) => (
-                <li key={key}>{element}</li>
-              ))}
-            </ul>
+            <Feedback feedBackArray={standardCTAFeedback}/>
           </div>
         </div>
       </div>
