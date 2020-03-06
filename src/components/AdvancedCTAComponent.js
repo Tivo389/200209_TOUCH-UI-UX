b82ea35c72bc2c8331fe039b86dd0ffa7e205105
Shun Miyaki <shunmiyaki@hotmail.com>
2020-03-06 00:19:16 -0800

    200306 - Chrome & Safari need different approach instead of :active. - Continue, either use a focus or add a class?

--- src/components/AdvancedCTAComponent.js
+++ src/components/AdvancedCTAComponent.js
@@ -11,18 +11,14 @@ class AdvancedCTAComponent extends Component {
       <div className="ctaContainer">
         <p>Advanced CTA</p>
         <a className="ctaAdvanced" href={`/anchorEvent${href}`}>
-          <span>Move to {href}</span>
+          Move to {href}
         </a>
         <Feedback feedBackArray={advancedCTAFeedback}/>
       </div>
+      // https://stackoverflow.com/questions/6406976/how-to-trigger-css-hover-state-using-javascript/6407065
+      // https://stackoverflow.com/questions/4347116/trigger-css-hover-with-js
+
     );
-  }
-  componentDidMount() {
-    const onFirstTouch = () => {
-      document.body.classList.add('touch');
-      window.removeEventListener('touchstart', onFirstTouch, false);
-    };
-    window.addEventListener('touchstart', onFirstTouch, false);
   }
 }
 
